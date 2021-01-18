3c1f56118238cbe73a7aa7ea0dc87b4c1db946e8
Olga Konopleva <olenenok.she@gmail.com>
2021-01-18 18:24:35 +0200

    Рефакторинг кода в класс

--- js/main.js
+++ js/main.js
@@ -1,43 +1,44 @@
-const timerRef = document.querySelector('#timer-1');
-const refs = {
-    days: document.querySelector('[data-value="days"]'),
-    hours: document.querySelector('[data-value="hours"]'),
-    mins: document.querySelector('[data-value="mins"]'),
-    secs: document.querySelector('[data-value="secs"]')
-}
+class CountdownTimer{
+    constructor({selector, targetDate}) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    }
+    
+    refs = {
+        days: document.querySelector('[data-value="days"]'),
+        hours: document.querySelector('[data-value="hours"]'),
+        mins: document.querySelector('[data-value="mins"]'),
+        secs: document.querySelector('[data-value="secs"]'),
+        timer: document.querySelector('#timer-1')
+    }
 
-// new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jul 17, 2019'),
-   
-//   });
-const targetDate = new Date('Feb 25, 2021');
-
-
-const timer = {
     start(){
         setInterval(() => {
             const currentDate = Date.now();
-            const deltaTime = targetDate - currentDate;
-            updateClockface(deltaTime);
-        }, 1000);
+            const deltaTime = this.targetDate - currentDate;
+            this.updateClockface(deltaTime);
+            }, 1000);
+        }
+
+    updateClockface (time) {
+        const days = Math.floor(time / (1000 * 60 * 60 * 24));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        this.refs.days.textContent = days;
+        this.refs.hours.textContent = hours;
+        this.refs.mins.textContent = mins;
+        this.refs.secs.textContent = secs;
     }
-}
 
-timer.start();
+    pad (value) {
+        return String(value).padStart(2,'0')
+    }
+  };
 
+  const timer = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Feb 25, 2021'),
+  });
 
-function updateClockface (time) {
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-    refs.days.textContent = days;
-    refs.hours.textContent = hours;
-    refs.mins.textContent = mins;
-    refs.secs.textContent = secs;
-}
-
-function pad (value) {
-    return String(value).padStart(2,'0')
-}
+  timer.start();
